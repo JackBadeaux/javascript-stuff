d08289336ef3801ee4a28109959e9431adf5a3df
JackBadeaux <jack.badeaux@mscoding.org>
2025-04-15 11:48:24 -0500

    Update app.js

--- js day 4/problems/app.js
+++ js day 4/problems/app.js
@@ -3,7 +3,6 @@
 //? let itemPrice = 10;
 //? let quantity = 10;
 //? const discount = .10;
-
 //? let subTotal = itemPrice * quantity;
 //? let discountAmount = subTotal * discount;
 //? let total = subTotal - discountAmount;
@@ -13,8 +12,6 @@
 // prompt that asks for celsius temperature
 // convert the input to fahrenheit
 // alert shows the result
-
-
 //? let input = prompt(`Celsius temperature`);
 //? let celsius = parseFloat(input);
 //? let fahrenheit = (celsius * 9/5) + 32;
@@ -25,7 +22,7 @@
 //? let str = "10";
 //? console.log(`number:`,num);
 //? console.log(`string:`,str);
-//? console.log(`str + num: the string and number merge into one:`,str + num);
+//? console.log(`str + num: the string and number merge into one:`,str + num); 
 //? console.log(`str - num: -, *, /, operators make the strings into numbers because they only work on numbers: ` , str - num);
 //? console.log(`num == str: this operator uses type coercion so it says they are the same,`,num == str);
 //? console.log(`num === str: this operator is the strict equality operator so it says false because one is a string and one is a number`,num === str);
