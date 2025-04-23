fa453a83e0da13f6ff7482876de893d7dc8d4f4b
JackBadeaux <jack.badeaux@mscoding.org>
2025-04-23 16:40:35 -0500

    js day 18 & 16 arrays and objects

---
+++ js day 16 intro to arrays/probs.js
@@ -0,0 +1,25 @@
+//! Prob 1
+//? let favoriteBooks = [`Gone`,`Dune`,`Lord of the Flies`,`To Kill a Mockingbird`,`1984`]
+//? console.log(favoriteBooks);
+//? favoriteBooks.push(`The Lord of the Rings`)
+//? console.log(`array with data pushed`,favoriteBooks);
+//? favoriteBooks.pop()
+//? console.log(`array with the last data popped`,favoriteBooks);
+//? favoriteBooks.unshift(`Dune 2`)
+//? console.log(`array with data added at the front with unshift`,favoriteBooks);
+//? favoriteBooks.shift()
+//? console.log(`array with data at front removed with shift`,favoriteBooks);
+
+//! Prob 2
+//? let color = [`red`, `green`,`blue`,`yellow`]
+//? let removedColor = color.pop()
+//? console.log(removedColor);
+//? console.log(color);
+
+//! Prob 3
+//? let names = [`James`,`Jack`,`John`,`Jake`,`Jeff`]
+
+//? for (let name of names) {
+//?     console.log(`Hello ${name}, how are you?`);
+  
+//? }
---
+++ js day 18 intro to objects/probs.js
@@ -0,0 +1,32 @@
+//! Prob 1
+//? let student ={
+//?     name: "John Smith",
+//?     age: 18,
+//?     grade: `A`,
+//? }
+//? console.log(student.name);
+//? console.log(student.grade);
+//? console.log(`whole student object`, student);
+
+//! Prob 2
+//? let student = {
+//?     name: "John Smith",
+//?     age: 18,
+//?     grade: `A`,
+//? }
+//? student.grade = `A+`
+//? student.university = "USM"
+//? console.log(student);
+//! Prob 3
+//? let car = {
+//?     make: `Toyota`,
+//?     model: `Camry`,
+//?     year: 2025,
+//? }
+//? let propertyName = `model`;
+//? console.log(car[propertyName]);
+//? let newProperty = `Color`;
+//? car[newProperty] = `Blue`
+//? console.log(car);
+
+
