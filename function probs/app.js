8c0dbd2f2743efa5cecf2bbc3b72c785570633c9
JackBadeaux <jack.badeaux@mscoding.org>
2025-05-01 22:15:22 -0500

    other prob

---
+++ function probs/app.js
@@ -0,0 +1,21 @@
+//! Prob 1 add two numbers func
+// function addNumbers(number1,number2){
+// return number1 + number2;
+// }
+// console.log(addNumbers(1,5));
+//! prob 2 greey user
+// function greetuser(name) {
+//     return `Hello, ${name}`
+// }
+// console.log(greetuser(`Jack`));
+//! prob 3 sum all numbers
+// function sumArray(array) {
+//     let total = 0
+//     for (let index = 0; index < array.length; index++) {
+//         total = total + array[index];
+//     }
+//     console.log(total);
+// }
+// sumArray([1, 2, 3, 4, 5])
+// sumArray([10,4,52,4,525,151,])
+//!
