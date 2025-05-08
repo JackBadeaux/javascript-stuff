0b41f7271b1b1c06aff24b2d6964fd168d3fc8f3
JackBadeaux <jack.badeaux@mscoding.org>
2025-05-08 12:11:51 -0500

    day 14 all probs

---
+++ js Day 14 Understanding Scope Global and Local Variables/prob.js
@@ -0,0 +1,23 @@
+// ! Prob 1
+// function claculateTotal() {
+//     let price = 100;
+//    return price
+// }
+// console.log(claculateTotal());
+// ! Prob 2
+// function outerFunction(){
+//     let text = `Hidden`;
+//     innerfunction(text)
+// }
+// function innerfunction(text) {
+//     console.log(text);
+// }
+// outerFunction()
+// ! Prob 3
+// let name1 = `Alice`;
+// function greet() {
+//    let name = `Bob`;
+//     console.log(name);   
+// }
+// greet()
+// console.log(name1);
