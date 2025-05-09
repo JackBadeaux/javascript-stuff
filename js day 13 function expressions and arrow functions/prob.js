7808e50fe6ce2ee0ab3a1350051702fdade73590
JackBadeaux <jack.badeaux@mscoding.org>
2025-05-09 11:02:54 -0500

    day 13

---
+++ js day 13 function expressions and arrow functions/prob.js
@@ -0,0 +1,17 @@
+// ! Prob 1
+// function subtract(num1,num2) {
+//     return num1 - num2
+// }
+// const subtract = function(num1, num2) {
+//     return num1 - num2
+// }
+// console.log(subtract(4, 5));
+// ! Prob 2
+// function multiply(num1, num2) {
+//     return num1 * num2
+// }
+// const multiply = (num1,num2) => num1 * num2
+// console.log(multiply(1,4));
+// ! Prob 3
+// const square = (num) => Math.pow(num,2)
+// console.log(square(6));
